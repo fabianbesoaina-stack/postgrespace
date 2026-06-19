e1e66f969155ad7ad92e30604ceb90709260df25
Darío Valenzuela <daro.valenzuelaa@gmail.com>
2026-06-19 11:53:48 -0400

    Create paso2.sql

---
+++ entregas/valenzuela_dario/paso2.sql
@@ -0,0 +1,15 @@
+CREATE TABLE mascotas (
+    id_mascota SERIAL PRIMARY KEY,
+    nombre     VARCHAR(50) NOT NULL,
+    especie    VARCHAR(30),
+    edad_meses INT,
+    tutor_id   INT,
+    CONSTRAINT fk_tutor
+        FOREIGN KEY (tutor_id)
+        REFERENCES tutores(id_tutor)
+        ON DELETE CASCADE  
+);
+
+INSERT INTO mascotas (nombre, especie, edad_meses, tutor_id) VALUES
+('Almendra', 'Gata', 108, 1),
+('Vanessa', 'Gata', 84, 2);
